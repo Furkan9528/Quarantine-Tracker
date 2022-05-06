a574efb98c3d5a1f37beb3112cd0a10af6ab8108
Furkan9528 <furkankara.9528@gmail.com>
2022-05-07 00:07:01 +0200

    Current location added

--- app/src/main/java/com/example/ppd/MainActivity.kt
+++ app/src/main/java/com/example/ppd/MainActivity.kt
@@ -4,22 +4,32 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var fusedLocationProviderClient : FusedLocationProviderClient
     private lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        fetchLocation()
 
         val findMap = findViewById<Button>(R.id.findMap)
 
@@ -66,6 +76,29 @@ class MainActivity : AppCompatActivity() {
                 111)
 
     }
+
+    private fun fetchLocation() {
+        val task: Task<Location> = fusedLocationProviderClient.lastLocation
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                101
+            )
+            return
+        }
+        task.addOnSuccessListener {
+            if(it != null ){
+                actualLocation.visibility = View.VISIBLE
+                actualLocation.setText("${it.latitude} \n ${it.longitude}")            }
+        }
+    }
+
+
 
     private fun setText(email:String?)
     {
