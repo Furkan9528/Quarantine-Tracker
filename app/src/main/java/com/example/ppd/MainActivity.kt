2d445ef65bdf1df0eb3fd537e0a8a9bff02109ca
Furkan9528 <furkankara.9528@gmail.com>
2022-05-06 20:46:26 +0200

    Latitude and Longitude of address in the database

--- app/src/main/java/com/example/ppd/MainActivity.kt
+++ app/src/main/java/com/example/ppd/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.ppd
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import androidx.core.app.ActivityCompat
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,13 +21,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
 
-
         val findMap = findViewById<Button>(R.id.findMap)
 
         findMap.setOnClickListener{
             val intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
         }
+
 
         val sharedPref=this?.getPreferences(Context.MODE_PRIVATE)?:return
         val isLogin=sharedPref.getString("Email","1")
@@ -55,6 +60,11 @@ class MainActivity : AppCompatActivity() {
             setText(isLogin)
         }
 
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                111)
+
     }
 
     private fun setText(email:String?)
@@ -69,8 +79,11 @@ class MainActivity : AppCompatActivity() {
                     address.text=tasks.get("Address").toString()
                     emailLog.text=tasks.get("email").toString()
 
+
                 }
         }
 
     }
+
+
 }
