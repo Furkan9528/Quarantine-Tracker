e9a6acd54d15faa78cbc3d00c8ba6acb1569f1e1
almighty00ZEDD <zeinbenzenati@gmail.com>
2022-06-23 16:05:07 +0200

    Added the case where the user is far from home, still needs the action that follows

--- app/src/main/java/com/example/ppd/CheckPositionWork.kt
+++ app/src/main/java/com/example/ppd/CheckPositionWork.kt
@@ -1,17 +1,35 @@
 package com.example.ppd
 
+import android.app.Activity
+import android.app.Application
 import android.app.NotificationManager
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.location.LocationManager
+import android.view.View
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class CheckPositionWork (appContext: Context, workerParams: WorkerParameters):
     Worker(appContext, workerParams) {
 
-    var channelId =  "channel1";
-    var notificationId  = 1;
+    val channelId =  "channel1"
+    val notificationId  = 1
+    val db= FirebaseFirestore.getInstance()
+    var LD_lat : Double =  0.0
+    var LD_lon : Double = 0.0
+    var AC_lat : Double =  0.0
+    var AC_lon : Double = 0.0
 
     var lm = appContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
     var gps_enabled = false
@@ -33,7 +51,16 @@ class CheckPositionWork (appContext: Context, workerParams: WorkerParameters):
         //si OK suivre sinon envoyer notif pour que le user le fasse lui meme
         if (gps_enabled && network_enabled) {
 
-        } else {
+            getActualLocation()
+            getLockDownLocation()
+
+            //l'utilisateur est loins de chez lui signaler l'incident
+            if(!isUserHome()){
+
+            }
+
+        }
+        else {
             val notif = NotificationCompat.Builder(applicationContext, channelId)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle("Demande de Position")
@@ -55,6 +82,57 @@ class CheckPositionWork (appContext: Context, workerParams: WorkerParameters):
         // Indicate whether the work finished successfully with the Result
         return Result.retry()
     }
+
+    //recupere la position du confinement depuis firebase
+    private fun getLockDownLocation()
+    {
+        val email =  FirebaseAuth.getInstance().currentUser?.email.toString()
+        if (email != null) {
+            db.collection("USERS").document(email).get()
+                .addOnSuccessListener {
+                        tasks->
+                    var city = tasks.get("Address").toString()
+                    var gc = Geocoder(applicationContext, Locale.getDefault())
+                    var addresses = gc.getFromLocationName(city,2)
+                    var address = addresses.get(0)
+                    this.LD_lon  =  address.longitude
+                    this.LD_lat = address.latitude
+                }
+        }
+    }
+
+    //recupere la position actuelle
+    private fun getActualLocation(){
+        val task: Task<Location> = LocationServices.getFusedLocationProviderClient(applicationContext).lastLocation
+        if(ActivityCompat.checkSelfPermission(applicationContext,android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(applicationContext,android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                Activity(),
+                arrayOf(
+                    android.Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                101
+            )
+            return
+        }
+        task.addOnSuccessListener {
+            if(it != null ){
+                this.AC_lon = it.longitude
+                this.AC_lat  = it.latitude
+            }
+        }
+    }
+
+    private fun  isUserHome() : Boolean{
+        val results = FloatArray(2)
+        Location.distanceBetween(AC_lat, AC_lon, LD_lat, LD_lon, results)
+        val distance = results[0]
+        val metre = (distance / 1000000).toInt()
+
+        return metre > 50
+    }
+
     }
 
 
