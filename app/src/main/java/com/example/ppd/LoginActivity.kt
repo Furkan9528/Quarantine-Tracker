e6b10494f2dbef675847f277fd4d9ad5c8c7bad2
Furkan9528 <furkankara.9528@gmail.com>
2022-06-25 18:30:06 +0200

    ForgotPassword is added

--- app/src/main/java/com/example/ppd/LoginActivity.kt
+++ app/src/main/java/com/example/ppd/LoginActivity.kt
@@ -1,9 +1,14 @@
 package com.example.ppd
 
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
@@ -27,6 +32,18 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+        mtpo.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Mot de pass oublié")
+            val view = layoutInflater.inflate(R.layout.forgot_password,null)
+            val username= view.findViewById<EditText>(R.id.mtpo)
+            builder.setView(view)
+            builder.setPositiveButton("Reset", DialogInterface.OnClickListener { _, _ ->
+                forgotPassword(username)
+            })
+            builder.setNegativeButton("Ferme", DialogInterface.OnClickListener { _, _ ->  })
+            builder.show()
+        }
 
         Login.setOnClickListener {
             if(checking()){
@@ -49,6 +66,23 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun forgotPassword(username: EditText) {
+        if(username.text.toString().isEmpty()){
+            return
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(username.text.toString()).matches()){
+            return
+        }
+        auth.sendPasswordResetEmail(username.text.toString())
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    Toast.makeText(this,"Email envoyé",Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
     private fun checking():Boolean
     {
         if(Email.text.toString().trim{it<=' '}.isNotEmpty()
